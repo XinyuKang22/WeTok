f8abb818ae18068c56b90921c349684746d56288
Yuxin Hong <u6684233@anu.edu.au>
2021-09-04 00:17:01 +0800

    direct jump of Login

--- app/src/main/java/com/example/wetok/MainActivity.java
+++ app/src/main/java/com/example/wetok/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.wetok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+                intent = new Intent(MainActivity.this, FunctionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
