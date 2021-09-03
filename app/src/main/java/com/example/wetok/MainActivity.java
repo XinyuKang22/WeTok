9069fec80ac0b6b5e5421845bd656bfd2c1d11f8
Yuxin Hong <u6684233@anu.edu.au>
2021-09-04 00:34:55 +0800

    jump of Login with username password matching

--- app/src/main/java/com/example/wetok/MainActivity.java
+++ app/src/main/java/com/example/wetok/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnLogin;
+    private EditText etUser;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = null;
-                intent = new Intent(MainActivity.this, FunctionActivity.class);
-                startActivity(intent);
-            }
-        });
+        etUser = findViewById(R.id.et_user);
+        etPassword = findViewById(R.id.et_password);
 
-
-
+        btnLogin.setOnClickListener(this);
     }
+
+    public void onClick(View v) {
+        String username = etUser.getText().toString();
+        String password = etPassword.getText().toString();
+        Intent intent = null;
+
+        if (username.equals("joy") && password.equals("123456")) {
+            intent = new Intent(MainActivity.this, FunctionActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
