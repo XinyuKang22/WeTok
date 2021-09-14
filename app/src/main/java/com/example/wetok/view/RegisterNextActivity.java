9f7d0f129786233d45987444f30fd26cb65b763f
Xinyu Kang <u6120911@anu.edu.au>
2021-09-14 18:15:43 +1000

    ui change

--- app/src/main/java/com/example/wetok/view/RegisterNextActivity.java
+++ app/src/main/java/com/example/wetok/view/RegisterNextActivity.java
@@ -1,11 +1,15 @@
 package com.example.wetok.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,23 +18,39 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.wetok.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.hbb20.CountryCodePicker;
 
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Arrays;
 import java.util.Calendar;
 
 public class RegisterNextActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterNext";
 
     private Context context;
     private Button btn_loginAftReg;
     private RadioGroup btnG_gender;
     private EditText et_userName;
     private EditText et_age;
-    private String gender= "Not Applicable.";
-    private int age;
+    private CountryCodePicker ccp;
+    private EditText et_phoneNumber;
+    private AutocompleteSupportFragment autocompleteFragment;
+
     private String name;
+    // Initialize optional fields
+    private int age = 0;
+    private String gender= "Not Applicable";
+    private String phoneNumber = "";
+    private String location = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +61,62 @@ public class RegisterNextActivity extends AppCompatActivity {
         et_userName = findViewById(R.id.et_userName);
         et_age = findViewById(R.id.et_age);
         btnG_gender = findViewById(R.id.btnG_gender);
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        et_phoneNumber = (EditText) findViewById(R.id.et_phoneNumber);
+        ccp.registerCarrierNumberEditText(et_phoneNumber);
+        autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+        autocompleteFragment.setTypeFilter(TypeFilter.CITIES);
 
+        // Input user name
+        et_userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                name = charSequence.toString().trim();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // Input age
+        et_age.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                int ageInt;
+                try{
+                    ageInt = Integer.parseInt(charSequence.toString());
+                }
+                catch (NumberFormatException ex){
+                    Toast.makeText(context,"Please enter the age in right form.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (ageInt<0 || ageInt>120){
+                    Toast.makeText(context,"Please enter the right age.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                age = ageInt;
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // Input gender
         btnG_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -53,32 +128,41 @@ public class RegisterNextActivity extends AppCompatActivity {
             }
         });
 
+        // Input phone number
+        ccp.setPhoneNumberValidityChangeListener(new CountryCodePicker.PhoneNumberValidityChangeListener() {
+            @Override
+            public void onValidityChanged(boolean isValidNumber) {
+                if (!isValidNumber){
+                    Toast.makeText(context,"Please enter valid phone number.", Toast.LENGTH_LONG).show();
+                }else {
+                    phoneNumber = ccp.getFullNumberWithPlus();
+                }
+            }
+        });
+
+        // Input location
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                location = place.getName();
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
         btn_loginAftReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nameStr = et_userName.getText().toString().trim();
-                String ageStr = et_age.getText().toString();
-                if (nameStr.isEmpty()){
+                // Check mandatory field User Name
+                if (name.isEmpty()){
                     Toast.makeText(context,"Please enter user name.", Toast.LENGTH_LONG).show();
                     return;
                 }
-                name = nameStr;
-                if (!ageStr.isEmpty()){
-                    int ageInt;
-                    try{
-                        ageInt = Integer.parseInt(ageStr);
-                    }
-                    catch (NumberFormatException ex){
-                        Toast.makeText(context,"Please enter the age in right form.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-                    if (ageInt<0 || ageInt>120){
-                        Toast.makeText(context,"Please enter the right age.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-                    age = ageInt;
-                }
-                createNewUser(name,gender,age);
+                createNewUser(name,age,gender,phoneNumber,location);
                 FirebaseAuth fbAuth = FirebaseAuth.getInstance();
                 FirebaseUser user = fbAuth.getCurrentUser();
                 toMainPage(user);
@@ -91,28 +175,8 @@ public class RegisterNextActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void createNewUser(String name, String gender, int age) {
+    private void createNewUser(String name, int age, String gender, String phoneNumber, String location) {
         Toast.makeText(context,"UserName:"+name+" Gender:"+gender+" Age:"+age, Toast.LENGTH_LONG).show();
     }
 
-    public static class DatePickerFragment extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-        }
-
-    }
 }
