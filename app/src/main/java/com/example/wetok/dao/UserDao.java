30f067c754a81c7309be162b7261a5d628356370
u6684233 <u6684233@anu.edu.au>
2021-10-10 18:00:41 +0800

    adduser

--- app/src/main/java/com/example/wetok/dao/UserDao.java
+++ app/src/main/java/com/example/wetok/dao/UserDao.java
@@ -10,7 +10,8 @@ public class UserDao {
     public static List<User> users = info.getUsers();
 
     //TODO: 新用户注册addUser方法
-    public static void addUser(){
+    public static void addUser(User u){
+        users.add(u);
     }
 
     public static User findUserByEmail(String email) {
