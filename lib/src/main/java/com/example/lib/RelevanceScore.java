e5e2cde7d17e2f5b6d1e3ee47cea9769096900ff
Xinyu Kang <u6120911@anu.edu.au>
2021-10-18 00:02:39 +1100

    tf-idf algorithm version 1

--- lib/src/main/java/com/example/lib/RelevanceScore.java
+++ lib/src/main/java/com/example/lib/RelevanceScore.java
@@ -1,6 +1,7 @@
 package com.example.lib;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,76 +9,97 @@ public class RelevanceScore {
     // TODO: 连接搜索结果（posts)
     ArrayList<String> searchResults;
 
-    // TODO: 搜索结果总数
-    int N;
-
     // TODO: 搜索的tag放在一个list里
     ArrayList<String> query;
 
-    public static ArrayList<String> tokenizePost(String post){
-        String delimiters = "[-\\t,; .?!:@'\\[\\](){}_*/]";
-        String[] tokens = post.split(delimiters);
-        ArrayList<String> toReturn = new ArrayList<>();
-        for (String s : tokens){
-            if (!s.trim().isEmpty()){
-                toReturn.add(s.toLowerCase());
-            }
-        }
-        return toReturn;
+    // TODO: 所有posts
+    ArrayList<String> allPosts;
+
+
+    public static int getTermFrequency(String tag, String post){
+        return post.length() - post.toLowerCase().replaceAll(tag.toLowerCase(),"").length();
     }
 
-    public static void main(String[] args) {
-        ArrayList<String> tokens = tokenizePost("welcome home@! Smith's!");
-        for (String s : tokens){
-            System.out.println(s);
+    /**
+     *
+     * @param query: a list of query tokens
+     * @param allPosts: a list of all posts
+     * @return a map of query tokens and their corresponding document frequencies
+     */
+    public static Map<String, Integer> getDocumentFrequency(ArrayList<String> query, ArrayList<String> allPosts){
+
+        Map<String, Integer> documentFrequencies = new HashMap<>();
+        for (String s : query){
+            documentFrequencies.put(s, 0);
         }
+
+        for (String post : allPosts){
+            for (String s : query){
+                if (post.contains(s)) documentFrequencies.replace(s,documentFrequencies.get(s) + 1);
+            }
+        }
+        return documentFrequencies;
     }
 
+    /**
+     *
+     * @param query: a list of query tokens
+     * @param searchResults: a list of retrieved posts
+     * @param allPosts: a list of all posts
+     * @param N: the total number of posts
+     * @return a map of the retrieved posts and their tf-idf scores
+     */
+    public static Map<String, Float> getScore(ArrayList<String> query, ArrayList<String> searchResults,ArrayList<String> allPosts, int N){
+        Map<String, Integer> df_map = getDocumentFrequency(query, allPosts);
 
-    public Map<Integer, Float> score(ArrayList<String> searchResults, ArrayList<String> query){
+        Map<String, Float> scores = new HashMap<>();
+        for (String post : searchResults){
+            float score = 0;
 
-        // initialize a map of <post_id, tf-idf score>
-        Map<Integer, Float> scores = new HashMap<>();
-        for (int i = 0; i < N; i ++){
-            scores.put(i, 0F);
-        }
-
-        // accumulate the tf-idf scores for each tag in the query
-        for (String tag : query){
-
-            // initialize the document frequency to be 1 instead of 0
-            // will NOT affect the ranking results, but avoid the potential illegal denominator problem
-            int documentFrequency = 1;
-
-            // a map of <post_id, term_frequency>
-            Map<Integer, Integer> termFrequencies = new HashMap<>();
-
-            int id = 0;
-            for (String post : searchResults){
-                termFrequencies.put(id, 0);
-                id ++;
-                ArrayList<String> tokens = tokenizePost(post);
-
-                // accumulate the term frequency in this post
-                for (String token: tokens){
-                    if (token.equals(tag)) termFrequencies.replace(id, termFrequencies.get(id) + 1);
-                }
-
-                // if the tag appears in this post at least once, increase the document frequency by 1
-                if (termFrequencies.get(id) > 0) documentFrequency++;
+            for (String tag : query){
+                int tf = getTermFrequency(tag, post);
+                // modified tf-idf, added 1 in document frequency to avoid invalid denominator problem, will NOT affect the ranking result
+                float tf_idf = (float) (tf * Math.log(N / (1.0+df_map.get(tag))));
+                score = score + tf_idf;
             }
-
-            // calculate the inverse document frequency for this tag
-            float idf = (float) Math.log(N / (documentFrequency+0.0));
-
-            // update the accumulated tf-idf score for each post
-            for (Map.Entry<Integer, Float> entry : scores.entrySet()){
-                int tf = termFrequencies.get(entry.getKey());
-                float tf_idf = (tf * idf);
-                entry.setValue(entry.getValue() + tf_idf);
-            }
+            scores.put(post, score);
         }
         return scores;
     }
 
+    public static void main(String[] args) {
+        String[] searchResults = {
+                "Weekend family time ",
+                "Sundays at home ",
+                "Weekends expolring ",
+                "Weekends well spent ",
+                "London weekends ",
+                "Spent the weekend exploring with this little one ",
+                "Sundays at home ",
+                "Kicking off the Valentines weekend ",
+                "Bring on the weekend ",
+                "culture weekends exploring ",
+                "Spent the weekend exploring with this little one ",
+                "Sundays at home ",
+                "Weekends expolring ",
+                "Weekend family time ",
+                "Before \u0026 after weekend feeding ",
+                "A little weekend love from Australia ",
+                "Before \u0026 after weekend feeding ",
+                "Before \u0026 after weekend feeding ",
+                "Before \u0026 after weekend feeding ",
+                "Sundays at home ",
+                "Weekends well spent ",
+                "Before \u0026 after weekend feeding ",
+                "Weekends well spent ",
+                "I grew up in a family that took photos of everything "
+        };
+
+        String[] query = {"weekend","family"};
+
+        Map<String, Float> scores = getScore(new ArrayList<>(Arrays.asList(query)),new ArrayList<>(Arrays.asList(searchResults)),new ArrayList<>(Arrays.asList(searchResults)), searchResults.length);
+        for (Map.Entry<String, Float> entry : scores.entrySet()){
+            System.out.println(entry.getKey() + " ---- " + entry.getValue());
+        }
+    }
 }
